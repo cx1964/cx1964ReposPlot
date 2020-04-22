a781ea91e79f884e3791c27241871c9cea2349bd
cx1964 <cx1964@mail.com>
2020-04-22 16:53:55 +0200

    scripts tbv python env

--- vb_mathplotlib_subplots_py3.py
+++ vb_mathplotlib_subplots_py3.py
@@ -72,3 +72,27 @@ a[1][1].plot(x,np.log10(x))
 a[1][1].set_title('log')
 
 plt.show()
+
+
+# Voorbeeld5: 2 grafieken in 4 afbeelding (met subplot)
+# https://www.tutorialspoint.com/matplotlib/matplotlib_subplots_function.htm
+import matplotlib.pyplot as plt
+
+# subplots(nrows, ncols)
+fig,a =  plt.subplots(2,2)
+import numpy as np
+x = np.arange(1,5,0.1)
+a[0][0].plot(x,x*x)
+a[0][0].set_title('square')
+
+a[0][1].plot(x,np.sqrt(x))
+a[0][1].set_title('square root')
+
+a[1][0].plot(x,np.exp(x))
+a[1][0].set_title('exp')
+
+x=np.arange(0, 2*np.pi)
+a[1][1].plot(x,np.sin(x))
+a[1][1].set_title('cyclisch')
+
+plt.show()
