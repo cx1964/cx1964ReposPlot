b39f7379a86c4fc75f68dfe9e9797e533f4411d8
cx1964 <cx1964@mail.com>
2020-04-22 15:27:39 +0200

    met lambda definities

--- vb_mathplotlib_subplots_py3.py
+++ vb_mathplotlib_subplots_py3.py
@@ -6,7 +6,7 @@
 
 from matplotlib import pyplot as plt
 
-# Voorbeeld1
+# Voorbeeld1: 1 grafieken in een afbeelding
 x = [5,8,10]
 y = [12,16,6]
 plt.plot(x,y)
@@ -15,7 +15,7 @@ plt.ylabel('Y-as')
 plt.xlabel('X-as')
 plt.show()
 
-# Voorbeeld2
+# Voorbeeld2: 2 grafieken in een afbeelding
 #from matplotlib import pyplot as plt
 from matplotlib import style
 style.use('ggplot')
@@ -24,8 +24,25 @@ y1 = [0,1,4,9,16,25]
 x2 = x1
 y2 = [5,7,9,11,13,15]
 # can plot specifically, after just showing the defaults:
-plt.plot(x1,y1,linewidth=5, label='functie1', color='red')
-plt.plot(x2,y2,linewidth=5, label='functie2', color='green')
+plt.plot(x1,y1,linewidth=5, label='$f1(x)=x^2$', color='red')     # label maken gebruik van Latex express
+plt.plot(x2,y2,linewidth=5, label='$f2(x)=2x+5$', color='green')  # label maken gebruik van Latex express
+plt.title('Titel van de grafiek voorbeeld2')
+plt.ylabel('Y-as')
+plt.xlabel('X-as')
+plt.legend()
+plt.show()
+
+
+# Voorbeeld2: 2 grafieken in een afbeelding met gebruik van lamda functie
+#from matplotlib import pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+x= range(0, 50) # genereer x-waarden
+f1 = list(map(lambda xi: xi**2 + 2*xi +5, x)) # functie definitie mbv een lambda functie
+f2 = list(map(lambda xi: -2*xi+500, x))
+# can plot specifically, after just showing the defaults:
+plt.plot(x,f1,linewidth=5, label='$f1(x)=x^2+2x+5$', color='red')     # label maken gebruik van Latex express
+plt.plot(x,f2,linewidth=5, label='$f2(x)=-2x+500$'  , color='green')  # label maken gebruik van Latex express
 plt.title('Titel van de grafiek voorbeeld2')
 plt.ylabel('Y-as')
 plt.xlabel('X-as')
